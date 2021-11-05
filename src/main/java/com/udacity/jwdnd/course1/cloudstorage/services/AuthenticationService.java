acfdc336527abcbaf4ad3ddc13301b8dea42dc19
Imoh Obot <obotimoh20@gmail.com>
2021-11-05 14:16:52 +0100

    Login security configuration

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
@@ -0,0 +1,48 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.entity.User;
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Base64;
+
+@Service
+public class AuthenticationService implements AuthenticationProvider {
+    private final EncryptionService encryptionService;
+    private final UserMapper userMapper;
+
+    public AuthenticationService(EncryptionService encryptionService, UserMapper userMapper) {
+        this.encryptionService = encryptionService;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        User user = userMapper.findUser(username);
+        if(user != null) {
+            SecureRandom random = new SecureRandom();
+            byte[] key = new byte[16];
+            random.nextBytes(key);
+            String encodedKey = Base64.getEncoder().encodeToString(key);
+            String encryptedPassword = encryptionService.encryptValue(password, encodedKey);
+            if(user.getPassword().equals(encryptedPassword)) {
+                return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
